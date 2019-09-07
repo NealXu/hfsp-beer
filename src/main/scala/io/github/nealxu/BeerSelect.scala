e2780a98dc33b0cddad447e2209d95b3177c3ade
nealxu <xuqixq@sina.com>
2019-09-07 21:52:16 +0800

    finish form html page

---
+++ src/main/scala/io/github/nealxu/BeerSelect.scala
@@ -0,0 +1,10 @@
+package io.github.nealxu
+
+import javax.servlet.http._
+
+class BeerSelect extends HttpServlet {
+
+  override def doPost(req: HttpServletRequest, resp: HttpServletResponse): Unit =
+    super.doPost(req, resp)
+
+}
